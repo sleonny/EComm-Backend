eee1e40c5a475378a11087cd143611aa31f2e8ea
Sean L <leonardmsean@gmail.com>
2023-06-29 12:35:56 -0400

    built product routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const dbConfig = require("../config/database");
+const dbConfig = require("../config/connection");
 
 const sequelize = new Sequelize(
   dbConfig.database,
